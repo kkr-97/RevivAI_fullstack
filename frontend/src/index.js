c43b41b8d94324d6870b1f8ff9577fa0fa0f896c
kkr-97 <kollikeerthan07@gmail.com>
2024-10-07 08:42:54 +0530

    index.html to include Bootstrap, and updated index.js to use react-router-dom and added a new SignUp component"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignUp from "./components/SignUp";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
